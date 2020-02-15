c8536143503eed40f9bbb0ffeac5e19e20fe8f8b
Greg D <gregorydesmarais@gmail.com>
2020-02-15 16:14:06 -0500

    Fixed positioning on Cardboard Page

--- src/pages/Cardboard/index.js
+++ src/pages/Cardboard/index.js
@@ -40,58 +40,70 @@ class Cardboard extends Component {
     }
 
 
-render() {
-    return (
-        <Container>
-            <Jumbotron>
-                <h1 className='text-center'>Cardboard Report Generator</h1>
-            </Jumbotron>
-            <Form>
-                <Form.Row>
-                    <Col lg={5}>
-                        <Form.Group controlId="user">
-                            <Form.Label>Select User</Form.Label>
-                            <Form.Control onChange={this.handleInputChange} as="select" value="Choose...">
-                                <option value="">Select User</option>
-                                <option value="nihika7263@xmailsme.com">Greg</option>
-                                <option value="joe@test.com">Joe</option>
-                            </Form.Control>
-                        </Form.Group>
-                    </Col>
-                    <Col lg={5}>
-                        <Form.Group controlId="cab">
-                            <Form.Label>Cabinet Location</Form.Label>
-                            <Form.Control onChange={this.handleInputChange} placeholder="AZ-01" />
-                        </Form.Group>
-                    </Col>
-                </Form.Row>
-                <Form.Row>
-                    <Col lg={5}>
-                        <Form.Group controlId="customer">
-                            <Form.Label>Customer</Form.Label>
-                            <Form.Control onChange={this.handleInputChange} placeholder="Joe Customer" />
-                        </Form.Group>
-                    </Col>
-                    <Col lg={5}>
-                        <Form.Group controlId="dc">
-                            <Form.Label>Data Center</Form.Label>
-                            <Form.Control onChange={this.handleInputChange} placeholder="CLT" />
-                        </Form.Group>
-                    </Col>
-                </Form.Row>
-                <Form.Row>
-                    <Col lg={5}>
-                        <Form.Group controlId="items">
-                            <Form.Label>Items</Form.Label>
-                            <Form.Control onChange={this.handleInputChange} placeholder="Small Cardboard Box" />
-                        </Form.Group>
-                    </Col>
-                </Form.Row>
-                <Button variant="primary" type="submit" onClick={this.handleFormSubmit}>Submit</Button>
-            </Form>
-        </Container>
-    );
-}
+    render() {
+        return (
+            <Container>
+                <Jumbotron>
+                    <h1 className='text-center'>Cardboard Report Generator</h1>
+                </Jumbotron>
+                <Form>
+                    <Form.Row>
+                        <Col lg={1}/>
+                        <Col lg={5}>
+                            <Form.Group controlId="user">
+                                <Form.Label>Select User</Form.Label>
+                                <Form.Control onChange={this.handleInputChange} as="select" value="Choose...">
+                                    <option value="">Select User</option>
+                                    <option value="nihika7263@xmailsme.com">Greg</option>
+                                    <option value="joe@test.com">Joe</option>
+                                </Form.Control>
+                            </Form.Group>
+                        </Col>
+                        <Col lg={5}>
+                            <Form.Group controlId="cab">
+                                <Form.Label>Cabinet Location</Form.Label>
+                                <Form.Control onChange={this.handleInputChange} placeholder="AZ-01" />
+                            </Form.Group>
+                        </Col>
+                        <Col lg={1}/>
+                    </Form.Row>
+                    <Form.Row>
+                        <Col lg={1}/>
+                        <Col lg={5}>
+                            <Form.Group controlId="customer">
+                                <Form.Label>Customer</Form.Label>
+                                <Form.Control onChange={this.handleInputChange} placeholder="Joe Customer" />
+                            </Form.Group>
+                        </Col>
+                        <Col lg={5}>
+                            <Form.Group controlId="dc">
+                                <Form.Label>Data Center</Form.Label>
+                                <Form.Control onChange={this.handleInputChange} placeholder="CLT" />
+                            </Form.Group>
+                        </Col>
+                        <Col lg={1}/>
+                    </Form.Row>
+                    <Form.Row>
+                        <Col lg={1}/>
+                        <Col lg={5}>
+                            <Form.Group controlId="items">
+                                <Form.Label>Items</Form.Label>
+                                <Form.Control onChange={this.handleInputChange} placeholder="Small Cardboard Box" />
+                            </Form.Group>
+                        </Col>
+                        <Col lg={1}/>
+                    </Form.Row>
+                    <Form.Row>
+                        <Col lg={4} />
+                        <Col lg={4} className="text-center">
+                            <Button variant="primary" type="submit" onClick={this.handleFormSubmit}>Submit</Button>
+                        </Col>
+                        <Col lg={4} />
+                    </Form.Row>
+                </Form>
+            </Container>
+        );
+    }
 }
 
 export default Cardboard;

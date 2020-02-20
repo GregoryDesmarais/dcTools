7f02efde4cfdd9b10fd92086f13f19bfb5db9ec1
Greg D <gregorydesmarais@gmail.com>
2020-02-20 16:34:17 -0500

    More work on Handoff page. - Updated Template component for better formatting. - Continued to work on Handoff code.  Adding Items and saving to LS works. - Listing Items in the Edit section works. - Still working on editing/removing Items.

--- client/src/components/Template/index.js
+++ client/src/components/Template/index.js
@@ -1,27 +1,22 @@
 import React from "react";
 
 
-function writeInfo(item) {
+function writeInfo(item, i) {
     return (
-        <div>
-            <p><strong>{item.title}</strong></p>
-            <p>{item.body}</p>
-        </div>
-    )
-}
-
-function writeTicket(item) {
-    return (
-        <div>
-            <p><strong>{item.customer}</strong> - TID: {item.TID}</p>
-            <p>{item.body}</p>
+        <div key={i}>
+            <p><strong>{item.title} {item.TID ? " - TID: "+item.TID : ""}</strong></p>
+            <ul>
+                {item.body.split("\r\n").map((lineItem, i) => {
+                    return (<li key={i}>{lineItem}</li>)
+                })}
+            </ul>
         </div>
     )
 }
 
 function Template(props) {
     let date = new Date();
-    
+
     let announce = [];
     let events = [];
     let maint = [];
@@ -31,9 +26,8 @@ function Template(props) {
     let patch = [];
     let other = [];
     let notes = [];
-    
-    if(props.items.length > 0)
-    {
+
+    if (props.items.length > 0) {
         announce = props.items.filter(item => item.type === "announce");
         events = props.items.filter(item => item.type === "events");
         maint = props.items.filter(item => item.type === "maint");
@@ -46,19 +40,20 @@ function Template(props) {
     }
 
     return (<div>
-        <div><strong>Date:</strong> {date.getMonth() + 1}/{date.getDate()}/{date.getFullYear()}</div>
-        <div><strong>Shift:</strong> {props.shift}</div>
-        <div><strong>Data Center:</strong> {props.dc}</div>
-        <div><strong>Local Ops Techs on Shift:</strong> {props.names}</div>
-        <p><strong>Announcements</strong>{announce ? announce.map(item => writeInfo(item)) : ""}</p>
-        <p><strong>Events</strong>{events ? events.map(item => writeInfo(item)) : ""}</p>
-        <p><strong>Scheduled Changes/Maintenance</strong>{maint ? maint.map(item => writeInfo(item)) : ""}</p>
-        <p><strong>Known Issues</strong>{issues ? issues.map(item => writeInfo(item)) : ""}</p>
-        <p><strong>Tape Rotations</strong>{tapes ? tapes.map(item => writeTicket(item)) : ""}</p>
-        <p><strong>Backups</strong>{backup ? backup.map(item => writeTicket(item)) : ""}</p>
-        <p><strong>Patching</strong>{patch ? patch.map(item => writeTicket(item)) : ""}</p>
-        <p><strong>All Other Tickets Worked</strong>{other ? other.map(item => writeTicket(item)) : ""}</p>
-        <p><strong>Notes</strong>{notes ? notes.map(item => writeInfo(item)) : ""}</p>
+        <div><h5>Date: {date.getMonth() + 1}/{date.getDate()}/{date.getFullYear()}</h5></div>
+        <div><h5>Shift: {props.shift}</h5> </div>
+        <div><h5>Data Center: {props.dc}</h5> </div>
+        <div><h5>Local Ops Techs on Shift: {props.names}</h5> </div>
+        <br /><br />
+        <div><h5>Announcements</h5>{announce ? announce.map((item, i) => writeInfo(item, i)) : ""}</div>
+        <div><h5>Events</h5>{events ? events.map((item, i) => writeInfo(item, i)) : ""}</div>
+        <div><h5>Scheduled Changes/Maintenance</h5>{maint ? maint.map((item, i) => writeInfo(item, i)) : ""}</div>
+        <div><h5>Known Issues</h5>{issues ? issues.map((item, i) => writeInfo(item, i)) : ""}</div>
+        <div><h5>Tape Rotations</h5>{tapes ? tapes.map((item, i) => writeInfo(item, i)) : ""}</div>
+        <div><h5>Backups</h5>{backup ? backup.map((item, i) => writeInfo(item, i)) : ""}</div>
+        <div><h5>Patching</h5>{patch ? patch.map((item, i) => writeInfo(item, i)) : ""}</div>
+        <div><h5>All Other Tickets Worked</h5>{other ? other.map((item, i) => writeInfo(item, i)) : ""}</div>
+        <div><h5>Notes</h5>{notes ? notes.map((item, i) => writeInfo(item, i)) : ""}</div>
     </div>
     );
 }

2224814f48879bc3fcb1fe13b6ddd01f5c15cc9a
Greg D <gregorydesmarais@gmail.com>
2020-02-21 03:01:44 -0500

    Updated NavBar with active location. - Added createBrowserHistory in App.js - Passing pathname to NavBar component - Added logic to add active class to Nav.Link components on appropriate pages

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -2,16 +2,16 @@ import React from "react";
 import Nav from 'react-bootstrap/Nav'
 import Navbar from "react-bootstrap/Navbar"
 
-function NavBar() {
+function NavBar(props) {
     return (
         <Navbar bg="dark" variant="dark">
             <Navbar.Brand href='/'>DC Tools</Navbar.Brand>
             <Nav className="mr-auto">
-                <Nav.Link href='/'>Home</Nav.Link>
-                <Nav.Link href='/handoff'>Shift Handoff</Nav.Link>
-                <Nav.Link href='/amber'>Amber Lights</Nav.Link>
-                <Nav.Link href='/cardboard'>Cardboard Tool</Nav.Link>
-                <Nav.Link href='/users'>Users</Nav.Link>
+                <Nav.Link href='/' className={props.path === "/" ? "active" : ""}>Home</Nav.Link>
+                <Nav.Link href='/handoff' className={props.path === "/handoff" ? "active" : ""}>Shift Handoff</Nav.Link>
+                <Nav.Link href='/amber' className={props.path === "/amber" ? "active" : ""}>Amber Lights</Nav.Link>
+                <Nav.Link href='/cardboard' className={props.path === "/cardboard" ? "active" : ""}>Cardboard Tool</Nav.Link>
+                <Nav.Link href='/users' className={props.path === "/users" ? "active" : ""}>Users</Nav.Link>
             </Nav>
         </Navbar>
     );
